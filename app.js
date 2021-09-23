8db5df850921995eeb6c113ea4c3a9251bf6abdb
HwanSu-K <kuma2301@naver.com>
2021-09-23 15:03:39 +0900

    Update: app.js delete console message

--- app.js
+++ app.js
@@ -46,8 +46,7 @@ app.use((err, req, res, next) => {
     message: err.message || '오류가 발생했습니다.',
   });
 });
-console.log(process.env.SEQUELIZE_USERNAME);
-console.log(process.env.SEQUELIZE_PASSWORD);
+
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
